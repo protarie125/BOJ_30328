42dc909e6431b5b99d0606055d5320562b52d688
protarie125 <protarie@gmail.com>
2023-11-06 18:13:27 +0900

    초기 커밋.

---
+++ BOJ_30328/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bit>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	cout << n * 4000;
+
+	return 0;
+}
